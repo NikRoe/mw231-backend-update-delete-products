e57d043a5f7fe2647049453d07ca83a05318c1c4
Niklas Röttger <niklas.roettger@outlook.de>
2023-04-19 17:43:51 +0200

    move state to homepage and use useswrmutation

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,40 @@
 import ProductList from "../components/ProductList";
 import styled from "styled-components";
 import ProductForm from "../components/ProductForm";
+import useSWRMutation from "swr/mutation";
 
 const Heading = styled.h1`
   text-align: center;
   color: var(--color-nemo);
 `;
 
+async function sendRequest(url, { arg }) {
+  const response = await fetch(url, {
+    method: "POST",
+    body: JSON.stringify(arg),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (response.ok) {
+    await response.json();
+  } else {
+    console.error(`Error: ${response.status}`);
+  }
+}
+
 export default function HomePage() {
+  const { trigger } = useSWRMutation("/api/products", sendRequest);
+
+  async function handleAddProduct(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const productData = Object.fromEntries(formData);
+
+    trigger(productData);
+  }
   return (
     <>
       <Heading>
@@ -16,7 +43,7 @@ export default function HomePage() {
         </span>
         Fish Shop
       </Heading>
-      <ProductForm />
+      <ProductForm onSubmit={handleAddProduct} />
       <hr />
       <ProductList />
     </>
