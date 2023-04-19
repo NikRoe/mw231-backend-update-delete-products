ab256f56e7d4155cfb22565b79eef4870391a1cd
Niklas Röttger <niklas.roettger@outlook.de>
2023-04-19 17:45:00 +0200

    update dynamic route to use useswrmutation

--- pages/[id].js
+++ pages/[id].js
@@ -1,9 +1,53 @@
 import Product from "../components/product";
+import { useRouter } from "next/router";
+import useSWRMutation from "swr/mutation";
+
+async function updateProduct(url, { arg }) {
+  const response = await fetch(url, {
+    method: "PUT",
+    body: JSON.stringify(arg),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (response.ok) {
+    await response.json();
+  } else {
+    console.error(`Error: ${response.status}`);
+  }
+}
 
 export default function ProductDetailsPage() {
+  const router = useRouter();
+  const {
+    query: { id },
+    push,
+  } = router;
+
+  const { trigger, isMutating } = useSWRMutation(
+    `/api/products/${id}`,
+    updateProduct
+  );
+
+  function handleEditProduct(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const updatedProduct = Object.fromEntries(formData);
+
+    trigger(updatedProduct);
+
+    push("/");
+  }
+
+  if (isMutating) {
+    return <h1>Submitting your changes</h1>;
+  }
+
   return (
     <>
-      <Product />
+      <Product onSubmit={handleEditProduct} />
     </>
   );
 }
